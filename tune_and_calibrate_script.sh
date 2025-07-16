051d01903579bbe8f91e6aa98c52e3131dedacd4
Sandro Hunziker <145295334+sandrohuni@users.noreply.github.com>
2025-07-16 11:15:16 +0200

    some more comits

--- tune_and_calibrate_script.sh
+++ tune_and_calibrate_script.sh
@@ -5,8 +5,8 @@
 set -e  # Exit on any error
 
 # Configuration
-CONFIG_DIR="../monthly_forecasting_models/SnowMapper_Based/Snow_GBT_LR"  # Path to model configuration directory
-MODEL_NAME="Snow_GBT_LR"  # Model name
+CONFIG_DIR="../monthly_forecasting_models/SnowMapper_Based/Snow_Correction"  # Path to model configuration directory
+MODEL_NAME="Snow_Correction"  # Model name
 INPUT_FAMILY="SnowMapper_Based"  # Input family for the model
 LOG_LEVEL="DEBUG"  # Set to INFO or DEBUG as needed
 
